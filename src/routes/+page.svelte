2c107287e6fa402182194d6cdda6ec2e969ede79
Makosai <kristopher.ali.dev@gmail.com>
2024-09-05 12:33:10 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">WebGL Showcases</h1>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><a href="/water">Water Simulation</a></li>
+		</ul>
+	</div>
+</div>
