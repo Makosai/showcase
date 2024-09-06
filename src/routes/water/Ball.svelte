0089fdb451aea7adc9f650f0ebc43d155469f894
Makosai <kristopher.ali.dev@gmail.com>
2024-09-06 15:29:39 -0400

    Setup a test environment to begin the simulation.

---
+++ src/routes/water/Ball.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+</script>
+
+<T.Group>
+	<T.Mesh position.y={1.25} receiveShadow castShadow>
+		<T.SphereGeometry args={[1]} />
+		<T.MeshStandardMaterial color="#487bfd" toneMapped={false} />
+	</T.Mesh>
+</T.Group>
---
+++ src/routes/water/Pool.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { DoubleSide, FrontSide } from 'three';
+</script>
+
+<T.Group>
+	<T.Mesh position.y={0.1} rotation.x={Math.PI / 2} receiveShadow castShadow>
+		<T.PlaneGeometry args={[8, 8]} />
+		<T.MeshStandardMaterial color="#dfdfdf" side={DoubleSide} />
+	</T.Mesh>
+	{#each Array(4) as _, i}
+		<!-- Create 4 walls around the plane -->
+		<T.Mesh
+			position={[i % 2 ? (i === 1 ? 4 : -4) : 0, 2.1, i % 2 ? 0 : i === 0 ? 4 : -4]}
+			rotation.y={i % 2 ? (i == 1 ? -Math.PI / 2 : Math.PI / 2) : i == 0 ? -Math.PI : 0}
+			receiveShadow
+			castShadow
+		>
+			<T.PlaneGeometry args={[8, 4]} />
+			<T.MeshStandardMaterial color="#dfdfdf" side={FrontSide} />
+		</T.Mesh>
+	{/each}
+</T.Group>
